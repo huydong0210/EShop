1c36a3d3324c25603798e8e2e2cefbf63667c689
iamphanh <85116700+iamphanh@users.noreply.github.com>
2023-07-12 08:33:07 +0700

    Version 0.1

---
+++ EShopFE/app/src/main/java/com/group2/eshopfe/ActivityLogo.java
@@ -0,0 +1,14 @@
+package com.group2.eshopfe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityLogo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+    }
+}
