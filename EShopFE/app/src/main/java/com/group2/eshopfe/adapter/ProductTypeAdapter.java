e29e9a60b281e90073a08426c66e3d65bdb6e118
iamphanh <85116700+iamphanh@users.noreply.github.com>
2023-07-16 12:54:13 +0700

    Create interface ProductType

--- EShopFE/app/src/main/java/com/group2/eshopfe/adapter/ProductTypeAdapter.java
+++ EShopFE/app/src/main/java/com/group2/eshopfe/adapter/ProductTypeAdapter.java
@@ -1,2 +1,41 @@
-package com.group2.eshopfe.adapter;public class ProductTypeAdapter {
+package com.group2.eshopfe.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.group2.eshopfe.modul.ProductType;
+
+import java.util.ArrayList;
+
+public class ProductTypeAdapter extends BaseAdapter {
+ ArrayList<ProductType> arrayList;
+ Context context;
+
+    public ProductTypeAdapter(ArrayList<ProductType> arrayList, Context context) {
+        this.arrayList = arrayList;
+        this.context = context;
+    }
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
 }
--- EShopFE/app/src/main/java/com/group2/eshopfe/modul/ProductType.java
+++ EShopFE/app/src/main/java/com/group2/eshopfe/modul/ProductType.java
@@ -1,2 +1,36 @@
-package com.group2.eshopfe.modul;public class ProductType {
+package com.group2.eshopfe.modul;
+public class ProductType {
+    public int id;
+    public String nameProductType;
+    public String imageProductType;
+
+    public ProductType(int id, String nameProductType, String imageProductType) {
+        this.id = id;
+        this.nameProductType = nameProductType;
+        this.imageProductType = imageProductType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameProductType() {
+        return nameProductType;
+    }
+
+    public void setNameProductType(String nameProductType) {
+        this.nameProductType = nameProductType;
+    }
+
+    public String getImageProductType() {
+        return imageProductType;
+    }
+
+    public void setImageProductType(String imageProductType) {
+        this.imageProductType = imageProductType;
+    }
 }
