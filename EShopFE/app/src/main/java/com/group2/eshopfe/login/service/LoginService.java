b91f96e8ddbe6e730da3dcafb434f45904456360
huydong <huydong0210@gmail.com>
2023-07-19 01:44:12 +0700

    update

--- EShopFE/app/src/main/java/com/group2/eshopfe/login/service/LoginService.java
+++ EShopFE/app/src/main/java/com/group2/eshopfe/login/service/LoginService.java
@@ -1,16 +1,7 @@
 package com.group2.eshopfe.login.service;
 
-import android.util.Log;
-import android.widget.Toast;
-
-import com.group2.eshopfe.api.ApiService;
-import com.group2.eshopfe.login.activity.LoginActivity;
 import com.group2.eshopfe.login.model.LoginRequest;
 import com.group2.eshopfe.payload.ResponseObject;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class LoginService {
     private LoginService(){
